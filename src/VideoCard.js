1aa4befc3ccbcb1da9e25e119aa9974a0cb7b805
Jimmy Lin <jimmydlin@gmail.com>
2020-09-04 09:03:11 -0700

    first upload

---
+++ src/VideoCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './VideoCard.css'
+
+function VideoCard() {
+    return (
+        <div>
+              <div className='videocard'>
+                    <img src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/a3liO9nUSCyEhyiAnDOw_Profit-with-JavaScript-new.jpg" alt=""/>
+                    <p>descriptions</p>
+                    <h2>title</h2>
+                    <p>number of likes</p>
+                </div>
+        </div>
+    )
+}
+
+export default VideoCard
